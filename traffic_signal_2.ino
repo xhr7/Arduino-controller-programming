dfe274478b23508e9e18ebf44db1d2a53268ae62
Rahaf <102740867+xhr7@users.noreply.github.com>
2022-09-03 02:06:36 +0300

    Add files via upload

---
+++ traffic_signal_2.ino
@@ -0,0 +1,18 @@
+void setup() {
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+
+}
+
+void loop() {
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+    delay(1000);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+    delay(1000);
+
+}
